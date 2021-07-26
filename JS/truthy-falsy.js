459aebbb3192e2166965f32520384eb9ab400ab2
Erick Marx <erickmarx5@gmail.com>
2021-07-26 01:04:00 -0300

    truthyfalse

---
+++ JS/truthy-falsy.js
@@ -0,0 +1,19 @@
+//  boolean
+const usuarioLogado = true
+const contapaga = false
+
+// truthy ou falsy
+
+// console.log(0 == false)
+// console.log('' == false)
+// console.log(1 == true)
+
+// null == vazio
+let minhaVar
+let varNull = null
+
+// console.log(minhaVar)
+// console.log(varNull)
+
+let numero = 3
+let texto = 'alura'
--- JS/type-bool.js
+++ JS/type-bool.js
@@ -6,7 +6,12 @@ const segundoNumero = 10
 // senha
 
 let cadastroAtivado = false;
-const texto1 = 'alura'
-const texto2 = 'Alura'
+// const texto1 = 'alura'
+// const texto2 = 'Alura'
+// console.log(texto1 === texto2)
 
-console.log(texto1 === texto2)
+
+const test1 = 0
+const test2 = false
+console.log(test1 == test2)
+
