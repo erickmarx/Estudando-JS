41b3f4c31e35b312822d78953d736c6ca1cf2868
Erick Marx <erickmarx5@gmail.com>
2021-07-28 09:45:20 -0300

    28Jul

---
+++ JS/Modulo2-Scripts/forEach.js
@@ -0,0 +1,8 @@
+const notas = [10,6,8,7]
+let somaNotas = 0
+
+notas.forEach(notas => {
+    somaNotas += notas
+})
+
+console.log(somaNotas/notas.length)
---
+++ JS/Modulo2-Scripts/forMedia.js
@@ -0,0 +1,8 @@
+const notas = [10,6,8,7]
+let somaNotas = 0
+
+for (let i = 0; i < notas.length; i++){
+    somaNotas += notas[i]
+    
+}
+console.log(somaNotas/notas.length)
