21c2f6dfc267a71ef6871c1f8838811608778756
Erick Marx <erickmarx5@gmail.com>
2021-07-29 00:40:25 -0300

    29Jul

--- JS/ProjetoPong-Modulo3/sketch.js
+++ JS/ProjetoPong-Modulo3/sketch.js
@@ -1,3 +1,5 @@
+//fazer player 2 ter velocidade propria e não seguir o eixo Y da bolinha
+
 let ball = [400,300,20,5,5] //X inicial, Y inicial, tamanho, X speed, Y speed
 let playersArray= [250,250,10,100] //Y player1, Y player2, largura, comprimento
 let pointsPlayers = [0,0] //pontos players
@@ -5,6 +7,7 @@ var foiPonto = 0 //correção de bug
 function setup() {
   createCanvas(800, 600);
 }
+
 
 function draw(){ // chamar funções
   background(0)
@@ -15,29 +18,31 @@ function draw(){ // chamar funções
   placar()
 }
 
+
 function players(){    //movimento dos players
   keyIsDown(UP_ARROW) ? playersArray.splice(0,1,playersArray[0]-5) : 0 //mover para cima o player1
     keyIsDown(DOWN_ARROW) ? playersArray.splice(0,1,playersArray[0]+5) : playersArray[0] //mover para baixo o player1
   
-  
-  if(ball[1] < 560 && ball[1] > 40){ //limitação de movimento do player 2
-  playersArray.splice(1,1,ball[1]-50) //movimento automatico do player 2
+  if(ball[1] < 510 && ball[1] > 0) { //limitação de movimento do player 2
+    playersArray.splice(1,1,ball[1]) //movimento automatico do player 2
   }
-  
-  
+
+
   rect(50, playersArray[0], playersArray[2], playersArray[3]) //player 1
   rect(750, playersArray[1], playersArray[2], playersArray[3]) //player 2
 }
   
+
 function movBall(){    //movimento da bolinha
   ball[0] = ball[0] + ball[3] //velocidade do X bolinha
   ball[1] = ball[1] + ball[4] //velociddade do Y bolinha
   
   if (ball[1] == height || ball[1] == height - height){
     ball.splice(4,1,ball[4]*-1)
-    // console.log(ball.splice(4,1,ball[4]*-1))
     }
 }
+
+
 function points(){    //pontos
     if (foiPonto == 0){ //correção de bug
   var hitP1 = collideRectCircle(50, playersArray[0], 10, 100, ball[0], ball[1], 20) //colisão player 1
@@ -52,9 +57,9 @@ function points(){    //pontos
   
   
   if (ball[0] >= width){ //ponto do lado esquerdo
-      ball.splice(3,1,ball[3]*-1) //rebater bolinha
-      pointsPlayers.splice(0,1,pointsPlayers[0]+1) //adicionar ponto
-      foiPonto = 1 //correção de bug
+    ball.splice(3,1,ball[3]*-1) //rebater bolinha
+    pointsPlayers.splice(0,1,pointsPlayers[0]+1) //adicionar ponto
+    foiPonto = 1 //correção de bug
   }
   
   if(ball[0] <= width - width){ //ponto do lado direito
@@ -63,6 +68,8 @@ function points(){    //pontos
     foiPonto = 1 //correção de bug
     }
 }
+
+
   function placar(){ //texto do placar
     fill(255)
     textSize(30)
