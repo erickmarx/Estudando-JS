0ea8e9119205c9691634583d15923410bf51d7b4
Erick Marx <erickmarx5@gmail.com>
2021-08-04 12:31:19 -0300

    04Ago

--- PetshopAPI-Modulo6/api/index.js
+++ PetshopAPI-Modulo6/api/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyparser = require('body-parser')
-// const config = require('config')
+const config = require('config')
 
 app.use(bodyparser.json())
 
 const roteador = require('./rotas/fornecedores')
 app.use('/api/fornecedores', roteador)
 
-app.listen(3000, () => console.log('funcionando'))
+app.listen(config.get('api.port'), () => console.log('funcionando'))
