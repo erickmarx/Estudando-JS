ff12f6693e7bf2b5e69b2fc35c38ef906b11fdde
Erick Marx <erickmarx5@gmail.com>
2021-08-08 02:15:44 -0300

    08Ago

--- PetshopAPI-Modulo8/nodejs-api-rest-aula-5/index.js
+++ PetshopAPI-Modulo8/nodejs-api-rest-aula-5/index.js
@@ -2,6 +2,7 @@ const customExpress = require('./config/customExpress')
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/tabelas')
 
+
 conexao.connect(erro => {
     if(erro) {
         console.log(erro)
--- PetshopAPI-Modulo8/nodejs-api-rest-aula-5/models/pets.js
+++ PetshopAPI-Modulo8/nodejs-api-rest-aula-5/models/pets.js
@@ -3,14 +3,15 @@ const uploadDeArquivos = require('../arquivos/uploadDeArquivos')
 
 
 class Pet {
-    adiciona(pet,res) {
-        const query = 'INSERT INTO  Pets SET ?'
-        uploadDeArquivos(pet.imagem, pet.nome, (erro, novoCaminho) => {
+    adiciona(data,res) {
+        console.log(data.pet.imagem)
+        const query = 'INSERT INTO Pets SET ?'
+        uploadDeArquivos(data.pet.imagem, data.pet.nome, (erro, novoCaminho) => {
             if(erro){
                 res.status(400).json({erro})
             }else{
-            const novoPet = { nome: pet.nome, imagem: novoCaminho}
-            conexao.query(query, pet, erro => {
+            const novoPet = { nome: data.pet.nome, imagem: novoCaminho}
+            conexao.query(query, data.pet, erro => {
                 if(erro) {
                     res.status(400).json(erro)
                 } else{
