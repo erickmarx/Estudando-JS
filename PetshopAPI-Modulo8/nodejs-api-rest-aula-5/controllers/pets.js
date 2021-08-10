dbba23bb71a64922efac2bfc9e04ebf2a21d7027
Erick Marx <erickmarx5@gmail.com>
2021-08-09 23:51:54 -0300

    09Ago

--- PetshopAPI-Modulo8/nodejs-api-rest-aula-5/controllers/pets.js
+++ PetshopAPI-Modulo8/nodejs-api-rest-aula-5/controllers/pets.js
@@ -6,4 +6,4 @@ module.exports = app => {
 
         Pet.adiciona(pet, res)
     })
-}
+} 
--- PetshopAPI-Modulo8/nodejs-api-rest-aula-5/models/pets.js
+++ PetshopAPI-Modulo8/nodejs-api-rest-aula-5/models/pets.js
@@ -3,16 +3,18 @@ const uploadDeArquivos = require('../arquivos/uploadDeArquivos')
 
 
 class Pet {
-    adiciona(data,res) {
-        console.log(data.pet.imagem)
+    adiciona(pet,res) {
         const query = 'INSERT INTO Pets SET ?'
-        uploadDeArquivos(data.pet.imagem, data.pet.nome, (erro, novoCaminho) => {
+
+        uploadDeArquivos(pet.imagem, pet.nome, (erro, novoCaminho) => {
             if(erro){
                 res.status(400).json({erro})
             }else{
-            const novoPet = { nome: data.pet.nome, imagem: novoCaminho}
-            conexao.query(query, data.pet, erro => {
+            const novoPet = { nome: pet.nome, imagem: novoCaminho}
+
+            conexao.query(query, novoPet, erro => {
                 if(erro) {
+                    console.log(erro)
                     res.status(400).json(erro)
                 } else{
                     res.status(200).json(novoPet)
