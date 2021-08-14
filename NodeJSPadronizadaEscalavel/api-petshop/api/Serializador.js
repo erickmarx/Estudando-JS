2da2c772d20a7682cdf4c4c9ab53e94c30665c19
Erick Marx <erickmarx5@gmail.com>
2021-08-14 19:49:55 -0300

    14Ago

--- NodeJSPadronizadaEscalavel/api-petshop/api/Serializador.js
+++ NodeJSPadronizadaEscalavel/api-petshop/api/Serializador.js
@@ -7,9 +7,32 @@ class Serializador{
 
     serializar(dados){
         if(this.contentType === 'application/json'){
-            return this.json(dados)
+            return this.json(this.filtrar(dados))
         }
         throw new ValorNaoSuportado(this.contentType)
+    }
+
+    filtrarObjeto(dados){
+        const novoObjeto = {}
+
+            this.camposPublicos.forEach((i) => {
+                if(dados.hasOwnProperty(i)){
+                    novoObjeto[i] = dados[i]
+                }
+            })
+        return novoObjeto
+    }
+
+    filtrar(dados){
+        if(Array.isArray(dados)){
+            dados = dados.map((item) => {
+                return this.filtrarObjeto(item)
+                
+            })
+        }else{
+            dados = this.filtrarObjeto(dados)
+        }
+        return dados
     }
 }
 
@@ -17,6 +40,11 @@ class SerializadorFornecedor extends Serializador{
     constructor(contentType){
         super()
         this.contentType = contentType
+        this.camposPublicos = [
+            'id',
+            'empresa',
+            'categoria'
+        ]
     }
 }
 
