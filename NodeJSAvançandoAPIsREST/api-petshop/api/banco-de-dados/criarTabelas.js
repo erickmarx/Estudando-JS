7d00efa4e5af14f0364835974c318f7f020a5829
Erick Marx <erickmarx5@gmail.com>
2021-08-16 17:33:26 -0300

    for invés de foreach

--- NodeJSAvançandoAPIsREST/api-petshop/api/banco-de-dados/criarTabelas.js
+++ NodeJSAvançandoAPIsREST/api-petshop/api/banco-de-dados/criarTabelas.js
@@ -1,6 +1,24 @@
-const modeloTabela = require('../rotas/fornecedores/modeloTabelaFornecedor')
+const modelosTabela = [
+    require('../rotas/fornecedores/modeloTabelaFornecedor'),
+    require('../rotas/fornecedores/produtos/modeloTabelaFornecedor')
+]
+async function criarTabelas(){
+    for(let i = 0; i < modelosTabela.length; i++){
+        const modelo = modelosTabela[i]
+        await modelo.sync()
+    }
+}
 
-modeloTabela
-    .sync()
-    .then(() => console.log('Tabela criada com sucesso'))
-    .catch(console.log)
+criarTabelas()
+
+// const modelosTabela = [
+//     require('../rotas/fornecedores/modeloTabelaFornecedor'),
+//     require('../rotas/fornecedores/produtos/modeloTabelaFornecedor')
+// ]
+// function criarTabelas(){
+//     modelosTabela.forEach(async(i) => {
+//         const modelo = modelosTabela[i]
+//         await modelo.sync()
+// })}
+
+// criarTabelas()
---
+++ NodeJSAvançandoAPIsREST/api-petshop/api/rotas/fornecedores/produtos/modeloTabelaFornecedor.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize')
+const instancia = require('../../../banco-de-dados')
+
+const colunas = {
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    preco: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    estoque: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+    }
+}
+
+const opcoes = {
+    freezeTableName: true,
+    tableName:'produtos',
+    timestamps: true,
+    createdAt: 'dataCriacao',
+    updatedAt: 'dataAtualizacao',
+    version: 'versao'
+}
+
+module.exports = instancia.define('produtos', colunas, opcoes)
