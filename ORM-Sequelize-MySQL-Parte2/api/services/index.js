a3b9892ea252605e556128ac20d3581a2f93f940
Erick Marx <erickmarx5@gmail.com>
2021-08-24 23:58:38 -0300

    Refactor Services

---
+++ ORM-Sequelize-MySQL-Parte2/api/services/index.js
@@ -0,0 +1,9 @@
+const PessoasServices = require('./pessoasServices')
+const NiveisServices = require('./niveisServices')
+const TurmasServices = require('./turmasServices')
+
+module.exports = {
+    PessoasServices: PessoasServices,
+    NiveisServices: NiveisServices,
+    TurmasServices: TurmasServices
+}
