ce227ad9dd9154c6b0eeec73a3e166c7c7b9344d
Erick Marx <erickmarx5@gmail.com>
2021-08-29 02:18:32 -0300

    Create ObjEmObj.js

---
+++ Programacao-Objetos/ObjEmObj.js
@@ -0,0 +1,15 @@
+const listaCPF = {
+    nome: "teste",
+    idade: "1",
+    cpf: "12312312312",
+    fones: ["456456456","456789456789"],
+}
+
+listaCPF.dependentes = {
+    nome: "test1",
+    parentesco: "1"
+}
+
+listaCPF.dependentes.nome = "teste11"
+
+console.log(listaCPF)
