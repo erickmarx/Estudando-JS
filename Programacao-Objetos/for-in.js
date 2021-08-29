745ea4d4c70edf70b1f0439bab1f47b881e727d1
Erick Marx <erickmarx5@gmail.com>
2021-08-29 18:21:05 -0300

    Create for-in.js

---
+++ Programacao-Objetos/for-in.js
@@ -0,0 +1,24 @@
+const Cliente = {
+    nome: "teste",
+    idade: "1",
+    cpf: "12312312312",
+    fones: ["456456456","456789456789"],
+    dependentes: [{
+        nome: "test1",
+        parentesco: "1"
+    },
+    {
+        nome: "test2",
+        parentesco: "2"  
+    }]
+}
+
+let relatorio = ""
+
+for (let i in Cliente){
+    if(typeof Cliente[i] === 'object'){
+        relatorio += Cliente[i]
+    }
+}
+
+console.log(relatorio)
