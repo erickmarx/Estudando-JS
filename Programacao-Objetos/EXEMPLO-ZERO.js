3d4e1e8e192d1bbe9398ffa295a2d1e1af19a766
Erick Marx <erickmarx5@gmail.com>
2021-08-30 02:33:37 -0300

    Update EXEMPLO-ZERO.js

--- Programacao-Objetos/EXEMPLO-ZERO.js
+++ Programacao-Objetos/EXEMPLO-ZERO.js
@@ -23,13 +23,11 @@ ClientePoupanca.prototype.depositarPoup = function(valor){
 }
 
 
-// console.log(andre.hasOwnProperty("saldoPoup"))
-// console.log(ju.hasOwnProperty("saldoPoup"))
-// console.log(andre instanceof Cliente)
-// console.log(typeof andre)
-// console.log(typeof ju)
-// console.log(ju instanceof ClientePoupanca)
-// console.log(Function.prototype.isPrototypeOf(Cliente))
-// console.log(Cliente.constructor === Function)
-
-console.log(ju)
+console.log(andre.hasOwnProperty("saldoPoup"))
+console.log(ju.hasOwnProperty("saldoPoup"))
+console.log(andre instanceof Cliente)
+console.log(typeof andre)
+console.log(typeof ju)
+console.log(ju instanceof ClientePoupanca)
+console.log(Function.prototype.isPrototypeOf(Cliente))
+console.log(Cliente.constructor === Function)
