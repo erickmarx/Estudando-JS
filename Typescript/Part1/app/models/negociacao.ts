144a00c13c45688f2eea2c06bb1fe7a453da8233
Erick Marx <erickmarx5@gmail.com>
2021-09-01 11:47:06 -0300

    Refactor Date

--- Typescript/Part1/app/models/negociacao.ts
+++ Typescript/Part1/app/models/negociacao.ts
@@ -1,29 +1,18 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 export class Negociacao{
-    private _data
-    private _quantidade
-    private _valor
 
-    constructor(data: Date, quantidade: number, valor: number){
-        this._data = data
-        this._quantidade = quantidade
-        this._valor = valor
+    constructor(
+        private _data: Date,
+        public readonly quantidade: number,
+        public readonly valor: number){}
+
+    get volume(): number{
+        return this.valor * this.quantidade
     }
 
     get data(): Date{
-        return this._data
-    }
-    
-    get quantidade(): number{
-        return this._quantidade
-    }
-
-    get valor(): number{
-        return this._valor
-    }
-
-    get volume(): number{
-        return this._valor * this._quantidade
+        const data = new Date(this._data.getTime())
+        return data
     }
 
 }
