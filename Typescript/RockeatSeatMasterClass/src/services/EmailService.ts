416c9829b033f6c13a8ba5ffa96daedba868f675
Erick Marx <erickmarx5@gmail.com>
2021-09-14 22:43:40 -0300

    seila

--- Typescript/RockeatSeatMasterClass/src/services/EmailService.ts
+++ Typescript/RockeatSeatMasterClass/src/services/EmailService.ts
@@ -1,29 +1,27 @@
-interface IMailTo{
-    name: string
-    email: string
+interface IMailTo {
+  name: string;
+  email: string;
 }
 
-interface IMailMessage{
-    subject: string
-    body: string
-    attachs?: string[]
+interface IMailMessage {
+  subject: string;
+  body: string;
+  attachs?: string[];
 }
 
-interface IMailDTO{
-    to: IMailTo
-    message: IMailMessage
+interface IMailDTO {
+  to: IMailTo;
+  message: IMailMessage;
 }
 
-interface IMailService{
-    sendMail(request: IMailDTO): void
+interface IMailService {
+  sendMail(request: IMailDTO): void;
 }
 
-class EmailService implements IMailService{
-
-    sendMail({to, message}: IMailDTO){
-        console.log(`Email Enviado para ${to.name}: ${message.subject}`)
-    }
-
+class EmailService implements IMailService {
+  sendMail({ to, message }: IMailDTO) {
+    console.log(`Email Enviado para ${to.name}: ${message.subject}`);
+  }
 }
 
-export default EmailService
+export default EmailService;
