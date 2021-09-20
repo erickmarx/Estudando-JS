3f576e8fd0547b2841cb633c9a4ba1f3e0643a66
Gone <erickmarx5@gmail.com>
2021-09-20 15:19:53 -0300

    a

--- NestJS/Part1/produtos-api/src/app.module.ts
+++ NestJS/Part1/produtos-api/src/app.module.ts
@@ -1,11 +1,26 @@
-import { ProdutosController } from './produtos.controller';
+import { Livro } from './livro.model';
+import { LivrosService } from './livro.service';
+import { LivrosController } from './livro.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProdutosController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Livro]),
+  ],
+  controllers: [AppController, LivrosController],
+  providers: [AppService, LivrosService],
 })
 export class AppModule {}
